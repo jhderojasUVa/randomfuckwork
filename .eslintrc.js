a376141fa85a4d40c52d6e40edeaa11332719290
Jesus A Hernandez de Rojas <jhderojas@gmail.com>
2023-03-25 16:49:14 +0000

    fix(): lint-staged added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,35 @@
 module.exports = {
-    settings: {
-        react: {
-            version: 'detect'
-        }
-    },
-    env: {
-        browser: true,
-        es2021: true
-    },
-    extends: ['plugin:react/recommended', 'standard'],
-    overrides: [],
-    plugins: ['react'],
-    rules: {
-        quotes: ['error', 'single'],
-        // we want to force semicolons
-        semi: ['error', 'always'],
-        // we use 2 spaces to indent our code
-        indent: ['error', 4],
-        // we want to avoid extraneous spaces
-        'no-multi-spaces': ['error'],
-        // new line at the end always
-        'eol-last': ['error', 'always'],
-        // no un used expresions (WARNING possible memory leak)
-        'no-unused-expressions': 'warn',
-        // comma operator
-        'no-sequences': 'off',
-        // no return needed
-        'no-return-assign': 'off',
-        // no break on case (WARNING)
-        'no-fallthrough': 'warn',
-        // no void
-        'no-void': 'off'
+  settings: {
+    react: {
+      version: 'detect'
     }
+  },
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [],
+  plugins: ['react'],
+  rules: {
+    quotes: ['error', 'single'],
+    // we want to force semicolons
+    semi: ['error', 'always'],
+    // we use 2 spaces to indent our code
+    indent: ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error'],
+    // new line at the end always
+    'eol-last': ['error', 'always'],
+    // no un used expresions (WARNING possible memory leak)
+    'no-unused-expressions': 'warn',
+    // comma operator
+    'no-sequences': 'off',
+    // no return needed
+    'no-return-assign': 'off',
+    // no break on case (WARNING)
+    'no-fallthrough': 'warn',
+    // no void
+    'no-void': 'off'
+  }
 };
--- src/App.js
+++ src/App.js
@@ -3,116 +3,134 @@ import axios from 'axios';
 import './App.css';
 
 function App () {
-    // States
-    const [data, setData] = useState([]);
-    const [color, setColor] = useState('dark');
-    const [wikipedia, setWikipedia] = useState(false);
-    const [isLoading, setLoading] = useState(false);
+  // States
+  const [data, setData] = useState([]);
+  const [color, setColor] = useState('dark');
+  const [wikipedia, setWikipedia] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
-    // Reload function
-    function pageReload () {
-        reDo();
+  // Reload function
+  function pageReload () {
+    reDo();
+  }
+
+  // Change from wikipedia to internal json
+  function changeFrom () {
+    if (wikipedia === true) {
+      setWikipedia(false);
+      pageReload();
+    } else {
+      setWikipedia(true);
+      pageReload();
     }
+  }
 
-    // Change from wikipedia to internal json
-    function changeFrom () {
-        if (wikipedia === true) {
-            setWikipedia(false);
-            pageReload();
-        } else {
-            setWikipedia(true);
-            pageReload();
-        }
+  // Change from dark to light theme
+  function changeMode () {
+    if (color === 'dark') {
+      setColor('light');
+    } else {
+      setColor('dark');
     }
+  }
 
-    // Change from dark to light theme
-    function changeMode () {
-        if (color === 'dark') {
-            setColor('light');
-        } else {
-            setColor('dark');
-        }
+  // Reload function (one who do the calls)
+  function reDo () {
+    if (wikipedia === true) {
+      setLoading(true);
+      fechWikiPediaData();
+    } else if (wikipedia === false) {
+      fetchData();
     }
+  }
 
-    // Reload function (one who do the calls)
-    function reDo () {
-        if (wikipedia === true) {
-            setLoading(true);
-            fechWikiPediaData();
-        } else if (wikipedia === false) {
-            fetchData();
-        }
-    }
-
-    // React hook for default start
-    useEffect(() => {
-        const fetchData = async () => {
-            const dataFetched = await axios.get('storage/arraystorage.json');
-
-            setData(dataFetched.data);
-        };
-        fetchData();
-    }, []);
-
-    // Fetch wikipedia data
-    const fechWikiPediaData = async () => {
-        const dataFetched = await axios.get('https://en.wikipedia.org/api/rest_v1/page/random/summary');
-
-        const stringTitle = { string: dataFetched.data.title };
-
-        setData([stringTitle]);
-        setLoading(false);
-    };
-
-    // Fetch JSON data
+  // React hook for default start
+  useEffect(() => {
     const fetchData = async () => {
-        const dataFetched = await axios.get('storage/arraystorage.json');
+      const dataFetched = await axios.get('storage/arraystorage.json');
 
-        setData(dataFetched.data);
+      setData(dataFetched.data);
     };
+    fetchData();
+  }, []);
 
-    let randomData = '';
-
-    // Select a random word or returned from Wikipedia
-    if (data.length > 0 && wikipedia === false) {
-        randomData = data[Math.floor(Math.random() * data.length)].string;
-    } else if (wikipedia === true) {
-        randomData = data[0].string;
-    } else if (isLoading === true) {
-        randomData = 'loading...';
-    }
-
-    // Show or hide (by CSS style) the loading image
-    const isVisible = isLoading === true ? 'visibilityYes' : 'visibilityNo';
-
-    // Change the theme
-    const appClass = 'App-header ' + color;
-
-    // Font size
-    const fontSize = {
-        fontSize: 'calc(100px - ' + randomData.length + 'px)'
-    };
-
-    return (
-        <div className="App">
-            <div className="App-menu">
-                <div className="App-where" onClick={changeFrom}>
-                    <p className={color}>{wikipedia ? 'Wikipedia' : 'Internal'}</p>
-                </div>
-                <div className="App-mode" onClick={changeMode}>
-                    <p className={color}>{color.toUpperCase()} mode</p>
-                </div>
-            </div>
-            <header className={appClass}>
-                <p onClick={pageReload} style={fontSize}>
-          FUCK <strong>{randomData}</strong>
-                </p>
-            </header>
-            <div className="App-loading">
-                <img className={isVisible} src="images/loading.gif" width="100" alt="loading... please wait!"/>
-            </div>
-        </div>
+  // Fetch wikipedia data
+  const fechWikiPediaData = async () => {
+    const dataFetched = await axios.get(
+      'https://en.wikipedia.org/api/rest_v1/page/random/summary'
     );
+
+    const stringTitle = { string: dataFetched.data.title };
+
+    setData([stringTitle]);
+    setLoading(false);
+  };
+
+  // Fetch JSON data
+  const fetchData = async () => {
+    const dataFetched = await axios.get('storage/arraystorage.json');
+
+    setData(dataFetched.data);
+  };
+
+  let randomData = '';
+
+  // Select a random word or returned from Wikipedia
+  if (data.length > 0 && wikipedia === false) {
+    randomData = data[Math.floor(Math.random() * data.length)].string;
+  } else if (wikipedia === true) {
+    randomData = data[0].string;
+  } else if (isLoading === true) {
+    randomData = 'loading...';
+  }
+
+  // Show or hide (by CSS style) the loading image
+  const isVisible = isLoading === true ? 'visibilityYes' : 'visibilityNo';
+
+  // Change the theme
+  const appClass = 'App-header ' + color;
+
+  // Font size
+  const fontSize = {
+    fontSize: 'calc(100px - ' + randomData.length + 'px)'
+  };
+
+  return (<
+    div className = "App" >
+    <
+      div className = "App-menu" >
+      <
+        div className = "App-where"
+        onClick = { changeFrom } >
+        <
+          p className = { color } > { wikipedia ? 'Wikipedia' : 'Internal' } < /p>{' '} <
+      /div>{' '} <
+        div className = "App-mode"
+        onClick = { changeMode } >
+        <
+          p className = { color } > { ' ' } { color.toUpperCase() }
+        mode { ' ' } <
+        /p>{' '} <
+      /div>{' '} <
+    /div>{' '} <
+      header className = { appClass } >
+      <
+        p onClick = { pageReload }
+        style = { fontSize } >
+        FUCK < strong > { randomData } < /strong>{' '} <
+      /p>{' '} <
+    /header>{' '} <
+      div className = "App-loading" >
+      <
+        img className = { isVisible }
+        src = "images/loading.gif"
+        width = "100"
+        alt = "loading... please wait!" /
+      >
+    <
+    /div>{' '} <
+  /div>
+  );
 }
 
 export default App;
